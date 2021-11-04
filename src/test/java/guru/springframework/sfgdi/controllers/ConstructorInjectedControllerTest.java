e65ec543734c9d6d39c3c84d2d7518db19762f8e
Emanuel <lorenzoemanueloscar@gmail.com>
2021-11-04 17:52:21 -0300

    Dependency Injection using Spring Framework

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/*
+Hay 3 dependency injected
+sin Spring
+el Setter, Property, Constructor
+En orden desde el peor y el mejor
+ */
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
